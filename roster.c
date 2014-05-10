7dcbafe7a9e166ebf65d0b2234636ef05ed6cb23
Jan Klemkow <j.klemkow@wemelug.de>
2014-05-10 13:10:12 +0200

    refactor roster

--- roster.c
+++ roster.c
@@ -1,9 +1,27 @@
+/*
+ * Copyright (c) Jan Klemkow <j.klemkow@wemelug.de>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 #include <fcntl.h>
 #include <limits.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+#include <sys/stat.h>
 
 void
 usage(void)
@@ -13,18 +31,19 @@ usage(void)
 }
 
 int
-main(int argc, char*argv[])
+main(int argc, char *argv[])
 {
+	FILE *fh = NULL;
 	int ch;
 	bool list_flag = false;
-	char path[_XOPEN_PATH_MAX];
+	char path_out[_XOPEN_PATH_MAX];
+	char path_in[_XOPEN_PATH_MAX];
 	char *dir = ".";
-	FILE *fh = NULL;
 
-	while ((ch = getopt(argc, argv, "l")) != -1) {
+	while ((ch = getopt(argc, argv, "bf:")) != -1) {
 		switch (ch) {
 		case 'l':
-			list_flag = 1;
+			list_flag = true;
 			break;
 		default:
 			usage();
@@ -34,14 +53,24 @@ main(int argc, char*argv[])
 	argc -= optind;
 	argv += optind;
 
-	snprintf(path, sizeof path, "%s/roster-%d", dir, getpid());
-	if ((fh = fopen(path, "r")) == NULL) goto err;
+	/* prepare fifo for answer from server */
+	snprintf(path_in, sizeof path_in, "%s/roster-%d", dir, getpid());
+	if (mkfifo(path_in, S_IRUSR|S_IWUSR) == -1) goto err;
 
-	if (list_flag == true) {
-		fprintf(fh, "");
-	}
+	/* send query to server */
+	snprintf(path_out, sizeof path_out, "%s/%s", dir, "in");
+	if ((fh = fopen(path_out, "w")) == NULL) goto err;
+	if (fprintf(fh,
+	    "<iq type='get' id='roster-%d'>"
+		"<query xmlns='jabber:iq:roster'/>"
+	    "</iq>", getpid()) == -1) goto err;
+	if (fclose(fh) == EOF) goto err;
 
-	if (fclose(fh) == -1) goto err;
+	/* read answer from server */
+	if ((fh = fopen(path_in, "r")) == NULL) goto err;
+	// TODO: mxml
+	if (fclose(fh) == EOF) goto err;
+	if (unlink(path_in) == -1) goto err;
 
 	return EXIT_SUCCESS;
  err:
